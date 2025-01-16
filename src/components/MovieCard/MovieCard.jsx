ac7d0f9b2f3810595fa4c0534a125f3588fb4305
Varvara00 <varvara.galy171203@gmail.com>
2025-01-16 15:13:23 +0100

    Finshed task react_movies-list-js

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,15 @@
 // MovieCard.js
 import './MovieCard.scss';
 
-export const MovieCard = ({ title, description, imageUrl, imdbUrl }) => (
+export const MovieCard = ({ movie }) => (
   <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img data-cy="MovieImage" src={imageUrl} alt="Film logo" />
+        <img
+          data-cy="MovieImage"
+          src={movie.imgUrl}
+          alt="Film logo"
+        />
       </figure>
     </div>
 
@@ -19,14 +23,20 @@ export const MovieCard = ({ title, description, imageUrl, imdbUrl }) => (
 
         <div className="media-content">
           <p className="title is-8" data-cy="MovieTitle">
-            {title}
+            {movie.title}
           </p>
         </div>
       </div>
 
       <div className="content">
-        <p data-cy="MovieDescription">{description}</p>
-        <a href={imdbUrl} data-cy="MovieLink">
+        <p data-cy="MovieDescription">
+          {movie.description}
+        </p>
+
+        <a
+          href={movie.imdbUrl}
+          data-cy="MovieLink"
+        >
           IMDB
         </a>
       </div>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,13 +6,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard
-        key={movie.imdbId}
-        title={movie.title}
-        description={movie.description}
-        imageUrl={movie.imgUrl}
-        imdbUrl={movie.imdbUrl}
-      />
+     <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
