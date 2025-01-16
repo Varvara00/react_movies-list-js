a128ddcd5826455b5cc253609d176a260e11b66a
Varvara00 <varvara.galy171203@gmail.com>
2025-01-16 15:02:08 +0100

    not finished task react_movies-list-js

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,18 @@
+import React from 'react';
+
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        title={movie.title}
+        description={movie.description}
+        imageUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+      />
+    ))}
+  </div>
+);
